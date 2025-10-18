46997b77194d8e3d979fb9b2e59c92e1b12ce326
Nexus <mike-hildman@outlook.com>
2025-10-18 09:04:04 -0400

    Remove server proxy and add path aliasing
    Removed server proxy configuration and added path aliasing.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:10000'
-    }
-  }
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
+
